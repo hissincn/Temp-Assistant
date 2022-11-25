262b537bf23e0af0dc268ec5b6f52db7cbca5da4
hissin <hissin@126.com>
2022-11-25 10:16:06 +0800

    1.2.4

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
